4e534bc0bd95b0142c42c8d431ee60e5a880ee32
Ghoul <31807465+Kitty-Therapist@users.noreply.github.com>
2020-07-30 19:42:26 -0400

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -6,20 +6,51 @@ import os
 import time
 import traceback
 import logging
+import shlex
 
 from discord.ext import commands
 from discord.ext.commands import BucketType, BadArgument
 from discord import abc, Forbidden, NotFound
 from discord.abc import PrivateChannel
+from discord import utils
 from utils import Util, Configuration
+from utils.Util import confirm_command
 from argparse import ArgumentParser
 
 class moderation(commands.Cog):
     def __init__(self, bot):
         pass
-    
+
     #@commands.Cog.listener()
     #async def on_message(self, message):
+    #    censor = Configuration.getConfigVar(message.guild.id, "CENSOR")
+    #    split = shlex.split(message.content.lower())
+    #    reaction = ["👍", "👎"]
+    #   if message.guild.id == 617944455303004163:
+    #        if "<@&634171428333158427>" in message.content:
+    #            for word in (w.lower() for w in censor):
+    #                if word in split:
+    #                    response = await message.channel.send("This is not a valid reason to ping the emergency role! Please refer to Riot's support page found in <#679877109040021568>'s pins.")
+    #                    await asyncio.sleep(15)
+    #                    await message.delete()
+    #                    await response.delete()
+    #                    return
+    #                else:
+    #                    embed = discord.Embed(title="You are about to ping the Emergency role", description="By pinging the Emergency role, you are about to summon our moderation team. **Are you sure that you are using the Emergency ping for the following reasons:**\n\n- Raid\n\n- Major spam\n\n- NSFW content", color=0xff7171)
+    #                    m = await message.channel.send(embed=embed)
+    #                    for name in reaction:
+    #                        emoji = utils.get(message.guild.emojis, name=name)
+    #                        await m.add_reaction(name)
+    #                        def check(reaction, user):
+    #                            if user.id == message.author.id and str(reaction.emoji) == '👍':
+    #                                await message.channel.send("Pinging the emergency role!")
+    #                            elif user.id == message.author.id and str(reaction.emoji) == '👎':
+    #                                await message.channel.send("Guess no pings today.")
+    #        else:
+    #            return
+    #    else:
+    #        return
+
     #    if message.guild.id == 679875946597056683:
     #        if message.author.id == 706269652724219987:
     #            return
@@ -44,8 +75,48 @@ class moderation(commands.Cog):
     #    else:
     #        return  
 
-    #This allows the moderator to pull the user into a private channel to discuss with them.
     @commands.command()
+    @commands.guild_only() 
+    @commands.cooldown(1, 600, BucketType.guild)   
+    @commands.bot_has_permissions(add_reactions=True)
+    async def emergency(self, ctx: commands.Context, *, reason=""):
+        """Uses the emergency ping!"""
+        bademergency = Configuration.getConfigVar(ctx.guild.id, "NONEMERGENCY")
+        emergencyrole = ctx.guild.get_role(Configuration.getConfigVar(ctx.guild.id, "EMERGENCY"))
+        if reason == "":
+            embed=discord.Embed(title="Invalid Emergency Reason!", description=f"It looks like that you did not provide a reason required for me to ping the Emergency ping. Please provide the correct reason before trying again!", color=0xfff952,timestamp=datetime.datetime.utcfromtimestamp(time.time()))
+            await ctx.send(embed=embed) 
+            return
+        
+        if any(word in reason.lower() for word in bademergency):
+            embed6=discord.Embed(title="Invalid Emergency Reason!", description=f"Hmmmm... Seems like you did not provide the valid reason for me to ping the emergency role. Here are the list of valid reasons to ping Emergency role!\n\n- Raid\n- NSFW content (porngraphy or gore)", color=0xfff952,timestamp=datetime.datetime.utcfromtimestamp(time.time()))
+            baduser = await ctx.send(embed=embed6)
+            await asyncio.sleep(15)
+            await baduser.delete()
+            await ctx.message.delete()
+            return
+
+        async def yes():
+            if emergencyrole is None:
+                embed3=discord.Embed(title="Unconfigured Emergency role!", description=f"Hmmmm... Seems like the emergency role is not configured. Please contact the mods over at the Modmail at <@711678018573303809>", color=0xfff952,timestamp=datetime.datetime.utcfromtimestamp(time.time()))
+                riprole = await ctx.send(embed=embed3)
+                await asyncio.sleep(15)
+                await riprole.delete()
+                await ctx.message.delete()
+                return
+
+            else:
+                await ctx.send(f"{emergencyrole.mention}, someone needs your assistance. Please ensure that this matter is solved appropriately.")
+                embed4=discord.Embed(title="Emergency Situation!", description=f"{ctx.author.mention} ({ctx.author.name}#{ctx.author.discriminator} (``{ctx.author.id}``) has pinged the Emergency role for ``{reason}``.", color=0xff9494, timestamp=datetime.datetime.utcfromtimestamp(time.time()))
+                await ctx.send(embed=embed4)
+                
+        embed2=discord.Embed(title="Emergency Ping Warning", description=f"Are you ABSOLUTELY sure that you want to ping the Emergency role for this reason: ``{reason}``?\n\nMake sure that the reason that you are pinging the emergency role meets the following:\n\n- Major raid\n- NSFW content in channel", color=0xfff952,timestamp=datetime.datetime.utcfromtimestamp(time.time()))
+        msg = await ctx.send(embed=embed2)
+        await confirm_command(ctx, msg, on_yes=yes)
+    
+
+    #This allows the moderator to pull the user into a private channel to discuss with them.
+    
     @commands.guild_only()
     @commands.bot_has_permissions(manage_roles=True)
     async def pull(self, ctx: commands.Context, member: discord.User, *, reason=""):
