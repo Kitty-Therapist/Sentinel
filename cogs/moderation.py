3f18ffd02bba25dddcc37a006ce486ba43f0e8d1
Ghoul <31807465+Kitty-Therapist@users.noreply.github.com>
2020-08-25 18:22:08 -0400

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -21,8 +21,8 @@ class moderation(commands.Cog):
     def __init__(self, bot):
         pass
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
+    #@commands.Cog.listener()
+    #async def on_message(self, message):
     #    censor = Configuration.getConfigVar(message.guild.id, "CENSOR")
     #    split = shlex.split(message.content.lower())
     #    reaction = ["👍", "👎"]
@@ -50,32 +50,6 @@ class moderation(commands.Cog):
     #            return
     #    else:
     #        return
-
-        if message.guild.id == 679875946597056683:
-            if message.author.id == 706269652724219987:
-                return
-            if 679879783630372865 in [role.id for role in message.author.roles]:
-                return
-            if 684144438251225099 in [role.id for role in message.author.roles]:
-                return
-            if 703063141990400001 in [role.id for role in message.author.roles]:
-                return
-            if 695765776086597663 in [role.id for role in message.author.roles]:
-                return
-            else:
-                split = shlex.split(message.content.lower())
-                censor = Configuration.getConfigVar(message.guild.id, "CENSOR")
-                for word in (w.lower() for w in censor):
-                    if word in split:
-                        response = await message.channel.send(f"Do not send any inappropriate language or non-permitted domains.")
-                        logging = message.guild.get_channel(Configuration.getConfigVar(message.guild.id, "LOGGING"))
-                        embed = discord.Embed(title=f"Filtered Message in Censor", description=f"Found message from {message.author.name}#{message.author.discriminator} (``{message.author.id}``) in {message.channel.mention} containing:\n\n```{message.content}```", color=0xff7171)
-                        await logging.send(embed=embed)
-                        asyncio.sleep(15)
-                        await message.delete()
-                        await response.delete()
-        else:
-            return  
 
     @commands.command()
     @commands.guild_only() 
