43e3a09cd19022d0c7abd498161a8a5b9aea517f
Ghoul <31807465+Kitty-Therapist@users.noreply.github.com>
2020-12-13 12:01:51 -0500

    hmm

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -165,7 +165,8 @@ class moderation(commands.Cog):
         pullroomrole = ctx.guild.get_role(Configuration.getConfigVar(ctx.guild.id, "PULLROOMROLE"))
         logs = ctx.guild.get_channel(Configuration.getConfigVar(ctx.guild.id, "PULLROOMLOG"))
         modrole = ctx.guild.get_role(Configuration.getConfigVar(ctx.guild.id, "MODROLE"))
-        user = await fetch_user(member.id)
+        verify = await fetch_user(member.id)
+        user = ctx.guild.get_member(verify.id)
         if reason == "":
             return await ctx.send("Please make sure to provide a reason why you're pulling the user in!")
         if modrole not in ctx.author.roles:
