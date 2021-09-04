14f8e282b4bfc8f60fa8a9fa79149f00320ed684
Ghoul <31807465+Kitty-Therapist@users.noreply.github.com>
2021-09-03 23:27:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Message } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES], commandPrefix:">>" });
 
 client.once('ready', () => {
     client.user.setStatus('online')
     client.user.setActivity('VALORANT', {type:'WATCHING'})
     console.log('Sentinel is ready to go!');
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply("I'm still alive! :heart:");
+    }
 });
 
 client.login(token);
