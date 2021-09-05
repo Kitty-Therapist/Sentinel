762171d9ae0e2b891919bea223ad213e467dd43e
Catdontpat <kirbyXstar@gmail.com>
2021-09-05 03:08:12 -0500

    Implementation of the Vote-Kick command (INCOMPLETE)

---
+++ src/events/messageCreate.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "messageCreate",
+    execute(client, message) {
+        // if (message.author.bot || !message.guild) return;
+
+
+    }
+} // This is the message event, Things that you'd usually do in v12 is now swapped from message -> messageCreate;
+
